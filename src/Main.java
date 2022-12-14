4aa500baefc0cf5a17cbec2e1d285c9d643ff08a
ruslankorotkov <ruslankorotkov.rus@yandex.ru>
2022-12-14 21:50:08 +0300

    стринг задания 1-2-3.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        String firstName = " Ivan ";
+        String middleName = " Ivanovich ";
+        String lastName = " Ivanov ";
+        String fullName = firstName + middleName + lastName;
+        System.out.println(" ФИО сотрудника — " + fullName);
+    }
+
+    public static void task2() {
+        System.out.println(" Задача 2");
+        String fullName = " Ivan  Ivanovich  Ivanov ";
+        String small = " Ivan  Ivanovich  Ivanov ";
+        System.out.println(small.toUpperCase());
+    }
+    public static void task3() {
+        System.out.println(" Задача 3 ");
+        String fullName = " Иванов Семён Семёнович ";
+        fullName = fullName.replace("ё","е");
+        System.out.println(" Данные ФИО сотрудника — " + fullName);
+
+
     }
 }
